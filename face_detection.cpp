e34f3d0548b346b4b8dc3ab790bb3d7984566911
Amy Huang <amyxjhuang@berkeley.edu>
2024-12-17 16:53:47 -0500

    Change to use frontalface default and tune

--- face_detection.cpp
+++ face_detection.cpp
@@ -9,9 +9,9 @@
 using namespace std;
 using namespace cv;
 
-void detectAndDisplay(Mat& img, CascadeClassifier& faceCascade, CascadeClassifier& nestedCascade, double scaleFactor);
+void detectAndDisplay(Mat& img, CascadeClassifier& faceCascade, CascadeClassifier& eyeglassesCascade, double scaleFactor);
 
-string cascadeName, nestedCascadeName;
+string cascadeName, eyeglassesCascadeName;
 
 
 int main(int argc, const char** argv) 
@@ -20,15 +20,14 @@ int main(int argc, const char** argv)
     Mat frame, image;
 
     CascadeClassifier faceCascade;
-    CascadeClassifier nestedCascade;
-    double scaleFactor = 1.1;
-    int minNeighbors = 2;
+    CascadeClassifier eyeglassesCascade;
+    double scaleFactor = 1.2;
+    int minNeighbors = 5;
 
     // Load classifiers from "opencv/data/haarcascades" directory 
-    nestedCascade.load( "opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml" ) ;
-
-    faceCascade.load( "opencv/data/haarcascades/haarcascade_frontalcatface.xml" ) ; 
-    capture.open(1); // Use macbook camera
+    eyeglassesCascade.load( "opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml" ) ;
+    faceCascade.load("opencv/data/haarcascades/haarcascade_frontalface_default.xml");
+    capture.open(1); // Use macbook camera, 0 for continuity camera
     if (capture.isOpened()) {
         cout << "Started detecting face" << endl;
         while (capture.read(frame)) {
@@ -37,7 +36,7 @@ int main(int argc, const char** argv)
                 break;
             
             Mat frame1 = frame.clone();
-            detectAndDisplay(frame1, faceCascade, nestedCascade, scaleFactor);
+            detectAndDisplay(frame1, faceCascade, eyeglassesCascade, scaleFactor);
             if (waitKey(10) == 'q')
                 break;
         }
@@ -46,7 +45,7 @@ int main(int argc, const char** argv)
     return 0;
 }
 
-void detectAndDisplay(Mat& img, CascadeClassifier& faceCascade, CascadeClassifier& nestedCascade, double scaleFactor) 
+void detectAndDisplay(Mat& img, CascadeClassifier& faceCascade, CascadeClassifier& eyeglassesCascade, double scaleFactor) 
 {
     vector<Rect> faces, faces2;
     Mat gray, smallImg;
@@ -58,23 +57,27 @@ void detectAndDisplay(Mat& img, CascadeClassifier& faceCascade, CascadeClassifie
     resize(gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT);
     equalizeHist(smallImg, smallImg);
     
-    faceCascade.detectMultiScale(smallImg, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+    int minNeighbors = 7;
+    faceCascade.detectMultiScale(smallImg, faces, 1.1, minNeighbors, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
 
     for (size_t i = 0; i < faces.size(); i++) {
         Rect r = faces[i];
         Mat smallImgROI;
         vector<Rect> nestedObjects;
         Point center;
-        Scalar color = Scalar(255, 0, 0);
+        Scalar blue = Scalar(255, 0, 0);
+        Scalar red = Scalar(0, 0, 255);
+        rectangle( img, r.tl()*scaleFactor, r.br()*scaleFactor, red, 3, 8, 0);
+        
 
         double aspect_ratio = (double)r.width/r.height;
-        if (!nestedCascade.empty() && aspect_ratio > 0.8 && aspect_ratio < 1.2) {
-            nestedCascade.detectMultiScale(smallImg(r), nestedObjects, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+        if (!eyeglassesCascade.empty() && aspect_ratio > 0.8 && aspect_ratio < 1.2) {
+            eyeglassesCascade.detectMultiScale(smallImg(r), nestedObjects, 1.1, 6, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
             for (size_t j = 0; j < nestedObjects.size(); j++) {
                 Rect nr = nestedObjects[j];
-                center.x = cvRound((r.x + nr.x + nr.width*0.5)*fx);
-                center.y = cvRound((r.y + nr.y + nr.height*0.5)*fx);
-                ellipse(img, center, Size(nr.width*0.5*fx, nr.height*0.5*fx), 0, 0, 360, color, 4, 8, 0);
+                center.x = cvRound((r.x + nr.x + nr.width*0.5)*scaleFactor);
+                center.y = cvRound((r.y + nr.y + nr.height*0.5)*scaleFactor);
+                ellipse(img, center, Size(nr.width*0.5*fx, nr.height*0.5*fx), 0, 0, 360, blue, 4, 8, 0);
             }
         }
     }
